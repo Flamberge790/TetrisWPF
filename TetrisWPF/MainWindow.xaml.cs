18284f174e45be8bb83c56dcaca968e180ee3e3a
dawod <dawid.dz4229@gmail.com>
2024-12-15 13:23:37 +0100

    test

--- TetrisWPF/MainWindow.xaml.cs
+++ TetrisWPF/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System;
+using System.Globalization;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -33,6 +35,46 @@ namespace TetrisWPF
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+    }
+    public class PolygonPointConverter : IMultiValueConverter
+        {
+            public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+            {
+                if (values.Length != 2 ||
+                    !double.TryParse(values[0]?.ToString(), out double width) ||
+                    !double.TryParse(values[1]?.ToString(), out double height))
+                {
+                    return null;
+                }
+
+            return new PointCollection([new Point(0, 0), new Point(width, 0), new Point(0, height)]);
+            }
+
+            public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+    public class PolygonPointConverter2 : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length != 2 ||
+                !double.TryParse(values[0]?.ToString(), out double width) ||
+                !double.TryParse(values[1]?.ToString(), out double height))
+            {
+                return null;
+            }
+
+
+            return new PointCollection([new Point(width, height), new Point(width, 0), new Point(0, height)]);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
         }
     }
 }
